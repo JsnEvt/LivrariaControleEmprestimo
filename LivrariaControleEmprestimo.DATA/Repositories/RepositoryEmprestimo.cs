f15b01fc02492ff7415d9fd7af49ae7363f3e6e2
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-11-07 08:18:19 -0300

    Interfaces/Repositories

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryEmprestimo.cs
@@ -0,0 +1,15 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    internal class RepositoryEmprestimo : RepositoryBase<Emprestimo>, IRepositoryEmprestimo
+    {
+        public RepositoryEmprestimo(bool SaveChanges = true) : base(SaveChanges) { };
+    }
+}
---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryLivro.cs
@@ -0,0 +1,15 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class RepositoryLivro : RepositoryBase<Livro>, IRepositoryLivros
+    {
+        public RepositoryLivro(bool SaveChanges = true) : base(SaveChanges) { }
+    }
+}
